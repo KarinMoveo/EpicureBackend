212cc19ba4a8b0bdaed51d1c06724f696d4aacf1
KarinMoveo <karino@moveo.co.il>
2023-09-12 12:03:59 +0300

    Starting the initial work

--- app.ts
+++ app.ts
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req : any, res: any) => {
-    res.send('Hello, Express!');
+    res.send('Hello, Express! This is the initial');
   });
   
   const port = process.env.PORT || 5000;
