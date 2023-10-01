adf8003bd0e786931e7a0956110a3991f04633e3
KarinMoveo <karino@moveo.co.il>
2023-10-01 08:21:06 +0300

    Change in date order

--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -24,7 +24,7 @@ export function filterRestaurants({
 	const restaurantOpeningYear = category === "New" ? 2020 : 0;
 
 	allRestaurants.forEach((restaurant: restaurant) => {
-		const [day, month, year] = restaurant.openingDate.split("-");
+		const [year, month, day] = restaurant.openingDate.split("-");
 		const isRestaurantCurrentlyOpen = category !== "Open Now" || isRestaurantOpen(restaurant.from, restaurant.to);
 
 		const restaurantRatingBitWise = 1 << (restaurant.popularity - 1);
